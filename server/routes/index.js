eb7dd4ade56a21093b5fa4d63df4e39b0abd35cc
Dawid Wisła <d.wisla@dreamlink.pl>
2024-11-12 18:56:28 +0100

    Add sitemap.xml

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const pages_url = require('../settings');
+const { SitemapStream, streamToPromise } = require('sitemap');
+
 
 router.get('/', async (req, res) => {
   try {
@@ -9,6 +11,29 @@ router.get('/', async (req, res) => {
   catch (error) {
   console.error(error);
   res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/sitemap.xml', async (req, res) => {
+  try {
+      const sitemap = new SitemapStream({ hostname: 'http://localhost:3000' });
+
+      // Dodaj poszczególne strony do mapy witryny
+      sitemap.write({ url: '/', changefreq: 'daily', priority: 1.0 });
+      sitemap.write({ url: '/services/monitoring', changefreq: 'monthly', priority: 0.8 });
+      sitemap.write({ url: '/services/network', changefreq: 'monthly', priority: 0.8 });
+      sitemap.write({ url: '/services/cloud', changefreq: 'monthly', priority: 0.8 });
+
+      // Dodaj inne adresy URL w podobny sposób
+
+      sitemap.end();
+
+      const sitemapXML = await streamToPromise(sitemap);
+      res.header('Content-Type', 'application/xml');
+      res.send(sitemapXML);
+  } catch (err) {
+      console.error(err);
+      res.status(500).end();
   }
 });
 
