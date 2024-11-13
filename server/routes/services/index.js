c93e47107df4b0b34979526e4578d9e8c25e41dc
Dawid Wisła <d.wisla@dreamlink.pl>
2024-11-13 11:22:20 +0100

    XD

--- server/routes/services/index.js
+++ server/routes/services/index.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pages_url = require('../../settings');
 
 
-// Endpoint do obsługi formularza
 router.get('/cloud', async (req, res) => {
     try {
       res.render(pages_url + '/services/cloud.ejs');
@@ -13,16 +12,6 @@ router.get('/cloud', async (req, res) => {
     res.status(500).send('Internal Server Error');
     }
   });
-
-// router.get('/containers', async (req, res) => {
-//   try {
-//     res.render(pages_url + '/services/containers.ejs');
-//   }
-//   catch (error) {
-//     console.error(error);
-//     res.status(500).send('Internal Server Error');
-//   }
-// });
 
 router.get('/monitoring', async (req, res) => {
   try {
@@ -41,6 +30,36 @@ router.get('/network', async (req, res) => {
   catch (error) {
     console.error(error);
     res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/fiber', async (req, res) => {
+  try {
+    res.render(pages_url + '/services/fiber.ejs');
+  }
+  catch (error) {
+  console.error(error);
+  res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/cctv', async (req, res) => {
+  try {
+    res.render(pages_url + '/services/cctv.ejs');
+  }
+  catch (error) {
+  console.error(error);
+  res.status(500).send('Internal Server Error');
+  }
+});
+
+router.get('/outsourcing', async (req, res) => {
+  try {
+    res.render(pages_url + '/services/outsourcing.ejs');
+  }
+  catch (error) {
+  console.error(error);
+  res.status(500).send('Internal Server Error');
   }
 });
 
