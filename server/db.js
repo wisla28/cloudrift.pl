f1f95b1fbe27f0d9117ead46cb994245842100ab
wisla28 <97619133+wisla28@users.noreply.github.com>
2025-02-04 21:31:50 +0100

    Delete server/db.js
    .

--- server/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-module.exports = function(app){
-    const dbURI = 'mongodb+srv://cloudriftdbuser:TBfJLK03zkhEFilt@cloudrift.rsnae.mongodb.net/?retryWrites=true&w=majority&appName=cloudrift'
-    mongoose.connect(dbURI);
-}
- 
